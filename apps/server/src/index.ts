1ae0a22f2407622b9c1ebec172092c911c681b0e
Bryan <sanbsbr@gmail.com>
2024-11-08 01:19:12 -0600

    feat: add db

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,15 +1,18 @@
 import bodyParser from 'body-parser'
 import express from 'express'
 import cors from 'cors'
+import { PrismaClient } from '../../../packages/database/prisma/prisma-client'
 
 const app = express()
+const prisma = new PrismaClient()
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/', (_, res) => {
-  return res.json({ ok: true })
+app.get('/', async (_, res) => {
+  const result = await prisma.product.findMany()
+  return res.json({ result })
 })
 
 const port = process.env.PORT || 5001
