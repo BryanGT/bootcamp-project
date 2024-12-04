4a1cffa454e4556aef3261f8ff5b2b3f947c247e
Bryan <sanbsbr@gmail.com>
2024-12-04 11:44:10 -0600

    chore: fix prisma client call

--- packages/database/scripts/cleanup/index.ts
+++ packages/database/scripts/cleanup/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../../prisma/prisma-client';
+import { PrismaClient } from '@prisma/client';
 import { cleanup } from './cleanup';
 
 const prisma = new PrismaClient();
--- packages/database/scripts/seed/seed.ts
+++ packages/database/scripts/seed/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../../prisma/prisma-client';
+import { PrismaClient } from '@prisma/client';
 import { faker } from '@faker-js/faker';
 
 export async function seed(prisma: PrismaClient) {
